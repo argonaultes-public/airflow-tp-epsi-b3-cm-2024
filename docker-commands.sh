01bb2d5896e50c6189430cabf1205de76bb5b5f3
argonaultes <gael@argonaultes.fr>
2024-11-25 14:01:39 +0100

    docker compose

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,1 +1,6 @@
+
+# créer le container airflowlocal depuis la commande run
 docker run --name airflowlocal -p 9090:8080  apache/airflow:latest standalone
+
+# créer les services décrits dans le fichier airflow-compose.yml grâce à la commande compose
+docker compose -f airflow-compose.yml up -d
