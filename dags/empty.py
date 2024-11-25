1a5192947b30354630c058ce59a649753935f408
argonaultes <gael@argonaultes.fr>
2024-11-25 12:16:32 +0100

    show 2 tasks in a row

--- dags/empty.py
+++ dags/empty.py
@@ -2,4 +2,6 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 with DAG(dag_id='empty'):
-    EmptyOperator(task_id='empty_task')
+    t1 = EmptyOperator(task_id='empty_task')
+    t2 = EmptyOperator(task_id='empty_task_2')
+    t1 >> t2
